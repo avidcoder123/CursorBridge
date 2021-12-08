0648c383ff044240d5acddabcbb14b47e085cb1a
MEER2008 <meerabdul2008@gmail.com>
2021-12-07 19:04:26 -0500

    more changes

---
+++ Components/Buttons/MoonButton.js
@@ -0,0 +1,27 @@
+document.getElementById("HTML").addEventListener("click", function(){
+    
+    if (document.getElementById("CSScontent").style.display == "block") {
+        document.getElementById("CSScontent").style.display = "none";
+    }
+    document.getElementById("HTMLcontent").style.display = "block";
+
+});
+
+document.getElementById("CSS").addEventListener("click", function(){
+
+        if (document.getElementById("HTMLcontent").style.display == "block") {
+            document.getElementById("HTMLcontent").style.display = "none";
+        }
+        document.getElementById("CSScontent").style.display = "block";
+    }
+);
+
+document.getElementById("IMG").addEventListener("click", function(){
+    
+        if (document.getElementById("HTMLcontent").style.display == "block") (document.getElementById("CSScontent").style.display == "block"); {
+            document.getElementById("HTMLcontent").style.display = "none";
+            document.getElementById("CSScontent").style.display = "none";
+        }
+        document.getElementById("Imgcontent").style.display = "block";
+    }
+);
