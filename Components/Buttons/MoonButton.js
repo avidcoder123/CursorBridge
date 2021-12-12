dc0cdea758809dbf109abe78323965ddbdb84c23
MEER2008 <meerabdul2008@gmail.com>
2021-12-12 18:53:38 -0500

    Now discoverpage

--- Components/Buttons/MoonButton.js
+++ Components/Buttons/MoonButton.js
@@ -1,7 +1,8 @@
 document.getElementById("HTML").addEventListener("click", function(){
     
-    if (document.getElementById("CSScontent").style.display == "block") {
+    if (document.getElementById("CSScontent").style.display == "block") (document.getElementById("Imgcontent").style.display = "block"); {
         document.getElementById("CSScontent").style.display = "none";
+        document.getElementById("Imgcontent").style.display = "none";
     }
     document.getElementById("HTMLcontent").style.display = "block";
 
@@ -9,8 +10,9 @@ document.getElementById("HTML").addEventListener("click", function(){
 
 document.getElementById("CSS").addEventListener("click", function(){
 
-        if (document.getElementById("HTMLcontent").style.display == "block") {
+        if (document.getElementById("HTMLcontent").style.display == "block") (document.getElementById("Imgcontent").style.display = "block"); {
             document.getElementById("HTMLcontent").style.display = "none";
+            document.getElementById("Imgcontent").style.display = "none";
         }
         document.getElementById("CSScontent").style.display = "block";
     }
