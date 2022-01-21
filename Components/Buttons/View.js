c06c201324a380316704ffbb65f824b90d60e3d4
MEER2008 <meerabdul2008@gmail.com>
2022-01-20 22:06:43 -0500

    added SVG tab (yay!)

--- Components/Buttons/View.js
+++ Components/Buttons/View.js
@@ -1,29 +1,53 @@
 document.getElementById("HTML").addEventListener("click", function(){
     
-    if (document.getElementById("CSScontent").style.display == "block") (document.getElementById("Imgcontent").style.display = "block"); {
-        document.getElementById("CSScontent").style.display = "none";
-        document.getElementById("Imgcontent").style.display = "none";
+        if (document.getElementById("CSScontent").style.display == 'block' || document.getElementById("Imgcontent").style.display == 'block' || document.getElementById("SVGcontent").style.display == 'block') {
+            document.getElementById("CSScontent").style.display = 'none';
+            document.getElementById("Imgcontent").style.display = 'none';
+            document.getElementById("SVGcontent").style.display = 'none';
+        }
+        document.getElementById("HTMLcontent").style.display = 'block';
+        
     }
-    document.getElementById("HTMLcontent").style.display = "block";
-
-});
+);
 
 document.getElementById("CSS").addEventListener("click", function(){
 
-        if (document.getElementById("HTMLcontent").style.display == "block") (document.getElementById("Imgcontent").style.display = "block"); {
-            document.getElementById("HTMLcontent").style.display = "none";
-            document.getElementById("Imgcontent").style.display = "none";
+        if (document.getElementById("HTMLcontent").style.display == 'block' ||
+        document.getElementById("Imgcontent").style.display == 'block'||
+        document.getElementById("SVGcontent").style.display == 'block') {
+            document.getElementById("HTMLcontent").style.display = 'none';
+            document.getElementById("Imgcontent").style.display = 'none';
+            document.getElementById("SVGcontent").style.display = 'none';
         }
-        document.getElementById("CSScontent").style.display = "block";
+        document.getElementById("CSScontent").style.display = 'block';
+        
     }
 );
 
 document.getElementById("IMG").addEventListener("click", function(){
     
-        if (document.getElementById("HTMLcontent").style.display == "block") (document.getElementById("CSScontent").style.display == "block"); {
-            document.getElementById("HTMLcontent").style.display = "none";
-            document.getElementById("CSScontent").style.display = "none";
+        if (document.getElementById("HTMLcontent").style.display == 'block' ||
+        document.getElementById("CSScontent").style.display == 'block' ||
+        document.getElementById("SVGcontent").style.display == 'block') {
+            document.getElementById("HTMLcontent").style.display = 'none';
+            document.getElementById("CSScontent").style.display = 'none';
+            document.getElementById("SVGcontent").style.display = 'none';
         }
-        document.getElementById("Imgcontent").style.display = "block";
+        document.getElementById("Imgcontent").style.display = 'block';
+        
     }
+);
+
+document.getElementById("SVG").addEventListener("click", function(){
+    
+    if (document.getElementById("HTMLcontent").style.display == 'block' ||
+     document.getElementById("CSScontent").style.display == 'block' ||
+    document.getElementById("Imgcontent").style.display == 'block') {
+        document.getElementById("HTMLcontent").style.display = 'none';
+        document.getElementById("CSScontent").style.display = 'none';
+        document.getElementById("Imgcontent").style.display = 'none';
+    }
+    document.getElementById("SVGcontent").style.display = 'block';
+    
+}
 );
